dc4cee0f566f687af7cd303d518c6882d419ffdf
Guilherme Betsa <ybtyza@gmail.com>
2024-02-26 00:29:26 -0300

    Adaptação para resoluçao de 1366px e 1920px

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import "./search.css"
+import "./searchCSS/search_1366px.css"
+import "./searchCSS/search_1920px.css"
 import  LogoA from '../assets/img/image-logo.png'
 import './expancaoMenu'
 
@@ -20,8 +21,8 @@ export const Search = () => {
                 <a href="#"><img src={LogoA} alt="Logo" style={{width: '30px', height: '30px'}}></img></a>
                 <input type="text" placeholder="Pesquisar"></input>
                 <i className='bx bx-search-alt'></i>
-            </form>
             <p id="horaAtual">{currentDateTime.toLocaleString()}</p>
+            </form>
             </>
     )
 }
