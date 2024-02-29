1b0b384c79a9f3c455a49dd64665621348df092e
Guilherme Betsa <ybtyza@gmail.com>
2024-02-28 21:39:07 -0300

    Inplementação de modal para descrições de imagens

---
+++ src/pages/perfil/layout/infoPerfil/descriçãoPublicações.js
@@ -0,0 +1,53 @@
+export const descricao_img = {
+    HTML: {
+        descricao_1: 'Conhecimento abrangente de tags HTML e suas propriedades.',
+        descricao_2: 'Capacidade de aplicar semântica de forma eficaz, utilizando elementos como <header>, <footer>, <article>, <section>, <nav>, entre outros.',
+        descricao_3: 'Competência em criar páginas acessíveis, garantindo a inclusão de atributos como alt para imagens e a marcação apropriada de elementos para serem compatíveis com leitores de tela.',
+        descricao_4: 'Proficiência em manipular o Document Object Model (DOM) usando JavaScript para criar interatividade dinâmica em páginas da web.',
+        descricao_5: 'Capacidade de criar formulários HTML complexos, incluindo validação de entrada, campos de entrada personalizados e técnicas avançadas de estilização.',
+        descricao_6: 'Compreensão de como estruturar o HTML de uma página para otimização de mecanismos de busca, incluindo o uso adequado de títulos, meta tags, headings e estrutura de links.',
+        descricao_7: 'Conhecimento sobre boas práticas de desempenho, como minimização do uso de recursos externos, compressão de arquivos e carregamento assíncrono de scripts.'
+    },
+    CSS: {
+        descricao_1: 'Entendimento dos seletores CSS, como seletores de tipo, classe, ID, atributo e pseudo-classes.',
+        descricao_2: 'Conhecimento sobre o modelo de caixa CSS e como ele afeta o layout e o dimensionamento dos elementos.',
+        descricao_3: 'Proficiência em layouts responsivos usando técnicas como media queries e unidades de medida relativas',
+        descricao_4: 'Compreensão dos conceitos de flexbox e grid layout para criação de layouts complexos e responsivos.',
+        descricao_5: 'Experiência em implementar temas escuros e claros, bem como suporte a temas personalizáveis, utilizando variáveis CSS e sistemas de design escaláveis.',
+    },
+    JavaScript: {
+        descricao_1: 'Conhecimento em manipulação de eventos e domínio do ciclo de vida do evento em aplicações web.',
+        descricao_2: 'Familiaridade com manipulação de arrays e objetos, incluindo métodos como map(), filter(), reduce() e Object.keys().',
+        descricao_3: 'Experiência com manipulação do DOM e atualização de elementos HTML usando vanilla JavaScript.',
+        descricao_4: 'Experiência em lidar com formulários e realizar validação de entrada de dados utilizando JavaScript.',
+        descricao_5: 'Experiência em trabalhar com estruturas de controle de fluxo de dados assíncronos, como loops e condicionais.',
+        descricao_6: 'Interesse em aprender e explorar bibliotecas e frameworks JavaScript populares, como React, Vue.js ou Angular.',
+    },
+    React: {
+        descricao_1: 'Volte mais tarde para mais informações.',
+        descricao_2: '',
+        descricao_3: '',
+        descricao_4: '',
+        descricao_5: '',
+        descricao_6: '',
+        descricao_7: '',
+    },
+    Python: {
+        descricao_1: 'Volte mais tarde para mais informações.',
+        descricao_2: '',
+        descricao_3: '',
+        descricao_4: '',
+        descricao_5: '',
+        descricao_6: '',
+        descricao_7: '',
+    },
+    PostgreSQL: {
+        descricao_1: 'Volte mais tarde para mais informações.',
+        descricao_2: '',
+        descricao_3: '',
+        descricao_4: '',
+        descricao_5: '',
+        descricao_6: '',
+        descricao_7: '',
+    }
+};
--- src/pages/perfil/layout/infoPerfil/publicacoes.js
+++ src/pages/perfil/layout/infoPerfil/publicacoes.js
@@ -1,35 +1,95 @@
 import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
 
-import './publicacoesCSS/publicacoes_1366px.css'
-import './publicacoesCSS/publicacoes_1920px.css'
+import './publicacoesCSS/publicacoes_1366px.css';
+import './publicacoesCSS/publicacoes_1920px.css';
 
-import Img_1 from '../../../../assets/img/publicacoes/img_1.png'
-import Img_2 from '../../../../assets/img/publicacoes/img_2.jpg'
-import Img_3 from '../../../../assets/img/publicacoes/img_3.png'
-import Img_4 from '../../../../assets/img/publicacoes/img_4.jpg'
-import Img_5 from '../../../../assets/img/publicacoes/img_5.jpg'
-import Img_6 from '../../../../assets/img/publicacoes/img_6.png'
+import { descricao_img } from './descriçãoPublicações';
+
+import Img_1 from '../../../../assets/img/publicacoes/img_1.png';
+import Img_2 from '../../../../assets/img/publicacoes/img_2.jpg';
+import Img_3 from '../../../../assets/img/publicacoes/img_3.png';
+import Img_4 from '../../../../assets/img/publicacoes/img_4.jpg';
+import Img_5 from '../../../../assets/img/publicacoes/img_5.jpg';
+import Img_6 from '../../../../assets/img/publicacoes/img_6.png';
 
 export const Publicacoes = () => {
-
-    const [publicacoes, setPublicacoes] = useState([
+    const [publicacoes] = useState([
         { titulo: 'HTML', url: Img_1 },
         { titulo: 'CSS', url: Img_2 },
-        { titulo: 'JavaScript', url: Img_3 },    
+        { titulo: 'JavaScript', url: Img_3 },
         { titulo: 'React', url: Img_4 },
         { titulo: 'Python', url: Img_5 },
         { titulo: 'PostgreSQL', url: Img_6 }
     ]);
 
+    const [modalIsOpen, setModalIsOpen] = useState(false); // Estado para controlar a abertura/fechamento do modal
+    const [selectedImage, setSelectedImage] = useState(null); // Estado para armazenar a imagem selecionada
+
     useEffect(() => {
         document.getElementById('numeroPublicacoes').innerHTML = `Publicações: <strong>${publicacoes.length}</strong>`;
     }, [publicacoes.length]);
 
+    // Função para abrir o modal com a imagem selecionada
+    const openModal = (image) => {
+        setSelectedImage(image);
+        setModalIsOpen(true);
+    };
+
+    // Função para fechar o modal
+    const closeModal = () => {
+        setSelectedImage(null);
+        setModalIsOpen(false);
+    };
+
     return (
         <section className="publicacoes" id="publicacoes">
-                {publicacoes.map((publicacao, index) => (
-                    <img key={index} src={publicacao.url} alt={`Publicação ${index + 1} - ${publicacao.titulo}`} />
-                ))}
+            {/* Mapeamento das publicações */}
+            {publicacoes.map((publicacao, index) => (
+                <img
+                    key={index}
+                    src={publicacao.url}
+                    alt={`Publicação ${index + 1} - ${publicacao.titulo}`}
+                    onClick={() => openModal(publicacao)} // Ao clicar na imagem, abre o modal com a imagem selecionada
+                />
+            ))}
+
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Visualização da Publicação"
+                className='modal'
+            >
+                {/* Conteúdo do modal */}
+                <div className='conteudoModal'>
+                    <img src={selectedImage?.url} alt={selectedImage?.titulo} />
+                    <div className='encapsulamento'>
+                        <h3>{selectedImage?.titulo}:</h3>
+                        {/* Acessando a descrição correta para a imagem selecionada */}
+                        {selectedImage && descricao_img[selectedImage.titulo] && (
+                            <div className='descricaoSkills'>
+                                {Object.keys(descricao_img[selectedImage.titulo]).map((key, index) => (
+                                    <p key={index}>{descricao_img[selectedImage.titulo][key]}</p>
+                                ))}
+                                <div className='modalComents'>
+                                    <input placeholder='Comentar'></input>
+                                    <i className='bx bxs-send'></i>
+                                    <div className='bx-i'>
+                                        <i className='bx bx-heart'></i>
+                                        <i className='bx bx-share-alt' ></i>
+                                        <i className='bx bx-bookmark'></i>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        )}
+                        <div className='comentarios'>
+                                    <p>Sem comentários...</p>
+                                </div>
+                        <button onClick={closeModal}><i className='bx bx-x'></i></button>
+                    </div>
+                </div>
+            </Modal>
         </section>
     )
 }
